89ae0e038b0aa602cd6dbe0d756eeb6b2410d2cd
Leo Vintila <lvintila@iMac-de-Leo.local>
2025-11-02 22:25:32 +0100

    restablecer

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
-// Nota: este archivo debe estar en la raíz del proyecto.
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
